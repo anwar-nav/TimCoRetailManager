620148a7a63946b352b0f078332bbd15aebc4f3b
Naveed Anwar <anwar.nav@yandex.com>
2021-04-05 14:38:02 +0500

    Add Swagger Documentation to API
    Adds Swashbuckle/Swagger to the API
    for documentation and testing.

---
+++ TRMDataManger/App_Start/AuthTokenOperation.cs
@@ -0,0 +1,65 @@
+﻿#if DEBUG
+
+using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace TRMDataManger.App_Start
+{
+    public class AuthTokenOperation : IDocumentFilter
+    {
+        /// <summary>
+        /// This method will add a new route of the API to its documentation with a post command.
+        /// </summary>
+        /// <param name="swaggerDoc"></param>
+        /// <param name="schemaRegistry"></param>
+        /// <param name="apiExplorer"></param>
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            swaggerDoc.paths.Add("/token", new PathItem
+            {
+                post = new Operation
+                {
+                    //Category to be used will be Auth
+                    tags = new List<string> { "Auth" },
+                    //Data being pass to post command should be of this type e.g. in message body
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-urlencoded"
+                    },
+                    //Definition of Parameters
+                    parameters = new List<Parameter>
+                    {
+                        new Parameter
+                        {
+                            type="string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formData",// this is the textbox created in the form and value provided will be used a parameter value
+                            @default = "password"
+                        },
+                        new Parameter
+                        {
+                            type="string",
+                            name = "username",
+                            required = false,
+                            @in = "formData"// this is the textbox created in the form and value provided will be used a parameter value
+                        },
+                        new Parameter
+                        {
+                            type="string",
+                            name = "password",
+                            required = false,
+                            @in = "formData"// this is the textbox created in the form and value provided will be used a parameter value
+                        },
+                    }
+                }
+            });
+        }
+    }
+}
+
+#endif
---
+++ TRMDataManger/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,36 @@
+﻿#if DEBUG
+
+using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace TRMDataManger.App_Start
+{
+    /// <summary>
+    /// This will add a parameter to every operation e.g get, post, put, delete
+    /// </summary>
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if (operation.parameters == null) //since the operator get has no parameter it will be null and not empty so comparison is made by ==
+            {
+                operation.parameters = new List<Parameter>(); //this instantiates the parameter and hence it will not remain null
+            }
+
+            operation.parameters.Add(new Parameter // new is used for instantiation
+            {
+                name = "Authorization",
+                @in = "header", //where we will put this parameter
+                description = "access token", //this will show up in documentation
+                required = false,
+                type = "string"
+            });
+        }
+    }
+}
+
+#endif
