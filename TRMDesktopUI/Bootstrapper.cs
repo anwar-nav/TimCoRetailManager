4fd5e7f7ca9b5094eb65bf7be8aea39025b58b0e
Naveed Anwar <anwar.nav@yandex.com>
2021-04-05 20:06:08 +0500

    Add WPF MVVM and SQL Data Project
    Adds WPF user interface project and configures it with Caliburn Micro for our MVVM framework. Also includes Simple Container Dependency Injection (DI) system. SQL data project is also added.

---
+++ TRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,79 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+    /// <summary>
+    /// This class will be used for setting up caliburn.micro
+    /// </summary>
+
+    public class Bootstrapper : BootstrapperBase
+    {
+        //This container will handle all the instantiating of classes aka Dependency Injection(DI).
+        private SimpleContainer _container = new SimpleContainer();
+
+        //Constructor used for base class method to run.
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        //This is where actual instantiation happens or container knows what to connect to what. The container holds the 
+        //instance of itself to pass out when SimpleContainer is called. This container is required in order to manipulate
+        //or to change or to get information out of it besides from constructor. This configure runs once at start of application.
+        //Added Singleton for Window Manager and Event Aggregator based in caliburn.micro. for managing windows and passing event messages.
+        //Added reflection (It's slow so string builder is better to use).
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+
+            _container
+                .Singleton<IWindowManager, WindowManager>() //Interface tied with implementation.
+                .Singleton<IEventAggregator, EventAggregator>(); //Interface tied with implementation.
+
+            GetType().Assembly.GetTypes() //reflection gettype of running assembly and get all the types for our current instance.
+                .Where(type => type.IsClass) //limit to type of class.
+                .Where(type => type.Name.EndsWith("ViewModel")) //limit to type of class with name ending ViewModel.
+                .ToList() // create a list of the found types
+                .ForEach(viewModelType => _container.RegisterPerRequest( // registers the each found class in container.
+                    viewModelType, viewModelType.ToString(), viewModelType));
+
+        }
+
+
+        //This method will override the base class method and be used on startup of application.
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //This will launch ShellViewModel on startup as our base view.
+            //Then this ShellViewModel will launch the ShellView.
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        //This method will override the base class method and be used for getting the instance.
+        protected override object GetInstance(Type service, string key)
+        {
+            //This will return the instance present in _container by having the type(service) of instance and name(key).
+            return _container.GetInstance(service, key);
+        }
+
+        //This method will override the base class method and be used for getting all the instances.
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            //This will return all the instances present in _container by having the type(service) of instance.
+            return _container.GetAllInstances(service);
+        }
+
+        //This is where things get constructed
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+
+    }
+}
