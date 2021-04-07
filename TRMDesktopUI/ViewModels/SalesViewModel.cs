3ddc455733727e17400d7bfe381c045d36a4a916
Naveed Anwar <anwar.nav@yandex.com>
2021-04-07 17:47:00 +0500

    Add Sales Page
    Adds the sales page xaml and the supporting view model. Just scaffolds out the view model, does not add logic.

---
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,143 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDesktopUI.ViewModels
+{
+    /// <summary>
+    /// This Model will be called by ShellViewModel and hence SalesView will be displayed on ShellView.
+    /// </summary>
+    public class SalesViewModel : Screen
+    {
+        //Declared private properties.
+        private BindingList<string> _products;
+        private BindingList<string> _cart;
+        private string _itemQuantity;
+
+        //ListBox Products
+        //Getter and Setter for declared private properties and raise property change event.
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set //Conditions can be applied here.
+            {
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        //ListBox Cart
+        //Getter and Setter for declared private properties and raise property change event.
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set //Conditions can be applied here.
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+        //Getter and Setter for declared private properties and raise property change event.
+        public string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set //Conditions can be applied here.
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => ItemQuantity);
+            }
+        }
+
+        //property that gets the value.
+        public string SubTotal
+        {
+            get
+            {
+                //TODO calculations
+                return "$0.00";
+            }
+        }
+
+        //property that gets the value.
+        public string Tax
+        {
+            get
+            {
+                //TODO calculations
+                return "$0.00";
+            }
+        }
+
+        //property that gets the value.
+        public string Total
+        {
+            get
+            {
+                //TODO calculations
+                return "$0.00";
+            }
+        }
+
+        //Button Checking AddToCart.
+        public bool CanAddToCart //Property that gets.
+        {
+            get
+            {
+                bool output = false;
+
+                //add something
+
+                return output;
+            }
+        }
+
+        //Button AddToCart.
+        public void AddToCart()
+        {
+            //add something
+        }
+
+        //Button Checking RemoveFromCart.
+        public bool CanRemoveFromCart //Property that gets.
+        {
+            get
+            {
+                bool output = false;
+
+                //add something
+
+                return output;
+            }
+        }
+
+        //Button RemoveFromCart.
+        public void RemoveFromCart()
+        {
+            //add something
+        }
+
+        //Button Checking CheckOut.
+        public bool CanCheckOut //Property that gets.
+        {
+            get
+            {
+                bool output = false;
+
+                //add something
+
+                return output;
+            }
+        }
+
+        //Button CheckOut.
+        public void CheckOut()
+        {
+            //add something
+        }
+    }
+}
